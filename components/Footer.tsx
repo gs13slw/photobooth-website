c5ecc31da8bb7ceaca460118963c3c8c97afcbe3
Sunni Williams <sunnilavasiettewilliams@gmail.com>
2026-08-15 03:59:59 -0400

    Fix typo in Footer.tsx import

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link fron "next/link";
+import Link from "next/link";
 import { Instagram, Facebook, Mail, Phone, MapPin } from "lucide-react";
 
 const QUICK_LINKS = [
